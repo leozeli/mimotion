a369d0fe05fd964770d13e868d07c291533a99cd
leozeli <leozeli@qq.com>
2022-07-03 19:00:59 +0800

    more step range

--- main.py
+++ main.py
@@ -572,11 +572,12 @@ if __name__ == "__main__":
 
     user_list = user.split('#')
     passwd_list = passwd.split('#')
-    setp_array = step.split('-')
+    step_list = step.split('#')
 
     if len(user_list) == len(passwd_list):
         to_push.push_msg = ''
-        for user, passwd in zip(user_list, passwd_list):
+        for user, passwd, step_ in zip(user_list, passwd_list, step_list):
+        	setp_array = step_.split('-')
             if len(setp_array) == 2:
                 step = str(random.randint(int(setp_array[0]), int(setp_array[1])))
                 print(f"已设置为随机步数（{setp_array[0]}-{setp_array[1]}）")
